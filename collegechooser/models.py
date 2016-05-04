56e158f61c793e62702a08b365153592018dc8e3
Leonhard Kuboschek <x32000@gmail.com>
2016-05-05 01:45:38 +0200

    First version of college chooser.

--- collegechooser/models.py
+++ collegechooser/models.py
@@ -1,3 +1,39 @@
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class UpdateWindow(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+    # Allocate first come first serve strategy
+    live_allocation = models.BooleanField(default=False)
+
+    # Only allow unallocated students to change records
+    only_unallocated = models.BooleanField(default=False)
+
+    def is_open(self):
+        now = timezone.now()
+        return self.start <= now < self.end
+
+    def can_update_colleges(self, profile):
+        if not self.is_open():
+            return False
+        else:
+            if self.only_unallocated and profile.college:
+                return False
+            else:
+                return True
+
+    def close(self):
+        # TODO Actually allocate people based on preferences
+        pass
+
+    def __str__(self):
+        if self.is_open():
+            return "Open until " + str(self.end)
+        else:
+            return "Ended at " + str(self.end)
--- collegechooser/views.py
+++ collegechooser/views.py
@@ -1,3 +1,30 @@
+import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import require_GET
 from django.shortcuts import render
 
+from .models import UpdateWindow
+
 # Create your views here.
+@require_GET
+def overview(request):
+    now = datetime.datetime.now()
+    profile = request.user
+
+    # Get currently active phase
+    windows = UpdateWindow.objects.filter(start__lte=now, end__gt=now).order_by('-start')
+    current_window = windows.first()
+
+    context = {
+        'window': current_window,
+        'profile': profile
+    }
+
+    if current_window:
+        context['can_change_college'] = current_window.can_update_colleges(profile)
+    else:
+        context['can_change_college'] = False
+
+    return render(request, 'overview.html', context)
