2af30f1f43570e29c2889d61af1352c6617170ce
bklw <x32000@gmail.com>
2016-05-12 18:15:03 +0200

    Add option to make many users tall / not tall.

--- roomer/admin.py
+++ roomer/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from roomer.models import UserProfile, RoommateRequest, Room, RoomTag, College
 
 
+def make_tall(modeladmin, request, queryset):
+    queryset.update(is_tall=True)
+make_tall.short_description = "Add tall flag to selected users"
+
+def unmake_tall(modeladmin, request, queryset):
+    queryset.update(is_tall=False)
+unmake_tall.short_description = "Remove tall flag from selected users"
+
 class PreferenceInline(admin.TabularInline):
     model = UserProfile.allocation_preferences.through
-
 
 class UserProfileAdmin(admin.ModelAdmin):
     search_fields = ['username', 'first_name', 'last_name']
@@ -12,6 +19,7 @@ class UserProfileAdmin(admin.ModelAdmin):
     model = UserProfile
 
     list_filter = ('college', 'old_college', 'major', 'year')
+    actions = [make_tall, unmake_tall]
 
     inlines = [
         PreferenceInline
