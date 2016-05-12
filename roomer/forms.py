5119b7bf4572ee6bc0614a3c31bfc03f9c48e618
Leonhard Kuboschek <x32000@gmail.com>
2016-05-12 10:58:18 +0200

    Add submit button. Improve home page.

--- roomer/forms.py
+++ roomer/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.forms import AuthenticationForm
 class RoomerAuthForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(RoomerAuthForm, self).__init__(*args, **kwargs)
-        self.fields['username'].label = "Campusnet username"
+        self.fields['username'].label = "campusnet username"
