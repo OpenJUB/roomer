f3a8a3bc86946d0f996504a59d0674d73d66e094
Tom Wiesing <tkw01536@gmail.com>
2017-03-29 17:50:19 +0200

    Update get_users command

--- roomer/management/commands/refresh_users.py
+++ roomer/management/commands/refresh_users.py
@@ -13,9 +13,35 @@ from getpass import getpass
 class Command(BaseCommand):
     help = 'Updates users from OpenJUB'
 
+    def add_arguments(self, parser):
+        parser.add_argument('-u', '--username', nargs='?', default=None,
+                            help='Username to use for LDAP. If omitted, ' +
+                                 'will ask for username interactively. ')
+        parser.add_argument('-p', '--password', nargs='?', default=None,
+                            help='Password to use for LDAP. If omitted, ' +
+                                 'will ask for password interactively. ')
+        parser.add_argument('-s', '--students', nargs='?', default=None,
+                            help='JSON file to read students from. See ' +
+                                 '\'manage.py export\'')
+        parser.add_argument('-c', '--courses', nargs='?', default=None,
+                            help='JSON file to read courses from. See ' +
+                                 '\'manage.py export\'')
+
+    def get_credentials(self, options):
+        if options["username"] is None:
+            self.stdout.write("Username: ")
+            user = input()
+        else:
+            user = options["username"]
+
+        if options["password"] is None:
+            pwd = getpass("Password for %s:" % user)
+        else:
+            pwd = options["password"]
+
+        return user, pwd
+
     def handle(self, *args, **options):
-        self.stdout.write("Username: ")
-        user = input()
-        pwd = getpass("Password for {}:".format(user))
+        user, pwd = self.get_credentials(options)
 
         ojub_auth.OjubBackend().refresh_users(user, pwd)
